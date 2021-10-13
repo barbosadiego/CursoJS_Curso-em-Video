108f7ae005e373013e21a90ae475622c9bf3dcf4
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-13 16:04:11 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 19.5
+console.log(`A velocidade do seu carro é de ${vel}km por hora`)
+if (vel > 60){
+  console.log('Você está acima da velocidade máxima permitida! MULTADO!')
+}
+console.log('Dirija com cuidade sempre!')
