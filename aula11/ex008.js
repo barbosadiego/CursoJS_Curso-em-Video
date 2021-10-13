a5557ad5b1f85a29d7a3efd33386211030d1f09b
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-13 16:06:31 -0300

    aula 11

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 19.5
+var vel = 60.1
 console.log(`A velocidade do seu carro é de ${vel}km por hora`)
-if (vel > 60){
+if (vel > 60){//condição simples
   console.log('Você está acima da velocidade máxima permitida! MULTADO!')
 }
 console.log('Dirija com cuidade sempre!')
