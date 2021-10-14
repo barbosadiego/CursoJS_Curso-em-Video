96d2b4d5c1bf72683cbfa63f51e177f286b35dd0
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-14 09:36:25 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,10 @@
 var idade = 15
-if (idade<18){
-  console.log('Menor de Idade')
+if (idade < 16){
+  console.log('Não vota')
 } else{
-  console.log('Maior de Idade')
+  if (idade < 18){
+    console.log('Voto opcional')
+  } else {
+    console.log('Voto obrigatório')
+  }
 }
