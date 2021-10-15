b669ce7521f79572e7ea6af4c636fc4128c137c4
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-15 14:40:58 -0300

    aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,20 @@
+// programa para mostar 'c' usando estrutura de repetição com variável de controle
+for(c=1;c<=5;c++){
+  console.log(c)
+}
+
+/* programa para mostar 'c' com teste log. no início
+var c = 1
+while (c <= 5){
+  console.log(c)
+  c++
+}
+*/
+
+/* programa para mostrar 'c' com teste lóg. no final 
+var c = 1
+do {
+  console.log(c)
+  c++
+} while (c <= 5)
+*/
