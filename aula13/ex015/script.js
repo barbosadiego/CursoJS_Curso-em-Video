793135c78854c7f33754a741f26609e3b2d96a03
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-15 11:08:05 -0300

    Update script.js

--- aula13/ex015/script.js
+++ aula13/ex015/script.js
@@ -1,16 +1,16 @@
 function verificar() {
   var data = new Date();
-  var ano = data.getFullYear();
-  var fAno = document.getElementById("txtano");
-  var res = document.querySelector("div#res");//forma alternativa de selecionar
+  var ano = data.getFullYear();//recebe ano com 4 dígitos
+  var fAno = document.getElementById("txtano");//recebe o valor do form
+  var res = document.querySelector("div#res");//forma alternativa de selecionar - manipula a div
   if (fAno.value.length == 0 || Number(fAno.value) > ano) {
     window.alert("[ERRO] Verifique os dados e tente novamente!");
   } else {
     var fsex = document.getElementsByName("radsex");
-    var idade = ano - Number(fAno.value);
+    var idade = ano - Number(fAno.value);//ano atual - ano de nascimento
     var genero = ''
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
+    var img = document.createElement('img')//cria tag img no html
+    img.setAttribute('id', 'foto')//insere a 'id=foto' na tag 'img'
     if (fsex[0].checked) {//se o 1º item for o selecionado
       genero = 'Homem'
       if (idade >=0 && idade <10){
