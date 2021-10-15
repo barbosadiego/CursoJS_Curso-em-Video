101bf2bc4f5dc96609a8ac0b200318ff92dce514
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-15 11:28:37 -0300

    aula 13a

---
+++ aula13a/ambiente.js
@@ -0,0 +1,16 @@
+
+var c = 1
+while (c <= 6) {
+  console.log(`tudo bem? - passo ${c}`)
+  c++ //c = c+1
+}
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
