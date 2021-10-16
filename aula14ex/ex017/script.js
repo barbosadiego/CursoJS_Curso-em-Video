2510242e64c112c09eaa063db790cf722c6c67ec
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-16 20:07:50 -0300

    correção exercicio

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,26 @@
+
+function tabuada(){
+  let num =document.getElementById('num')
+  let tab =document.getElementById('seltab')
+  if (num.value.length == 0){
+    window.alert('Por favor digite um número')
+  } else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''//limpa a tabuada antes de começar
+    while (c <= 10){
+      let item = document.createElement('option')//criando o elemento html 'option'
+      item.text = `${n} x ${c} = ${n*c}`
+      item.value = `tab${c}`//para outras linguagens
+      tab.appendChild(item)//adicionar elemento filho
+      c++
+    }
+  } 
+}
+
+/*
+código antes da correção
+
 var num = document.getElementById("num");
 var res = document.getElementById("res");
 var tab = ''
@@ -12,3 +35,4 @@ function tabuada() {
     }
   } res.innerHTML = tab
 }
+*/
