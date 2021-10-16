56b1144d9e123f91a204f21cefa796414bb14b9a
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-16 10:56:57 -0300

    correção do exercício

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,6 +2,41 @@ function contar() {
   var ini = document.getElementById("ini");
   var fim = document.getElementById("fim");
   var passo = document.getElementById("passo");
+  var res = document.getElementById("res");
+
+  if (
+    ini.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    //window.alert("[ERRO] Faltam dados!");
+    res.innerHTML = "Impossível contar!";
+  } else {
+    res.innerHTML = "Contando: <br>";
+    var i = Number(ini.value);
+    var f = Number(fim.value);
+    var p = Number(passo.value);
+    if (p <= 0) {
+      window.alert("Passo inválido! Considerando PASSO 1");
+      p = 1;
+    }
+    if (i < f) {
+      //contagem crescente
+      for (var c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`; //codigo para emoji no js '\u{emoji}'
+      }
+    } else {
+      //contagem decrescente
+      for (var c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`;
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`;
+  }
+
+  /* 
+  ********* códido antes da correção ****************
+
   var a = Number(ini.value);
   var b = Number(fim.value);
   var c = Number(passo.value);
@@ -18,4 +53,5 @@ function contar() {
     }
   }
   res.innerHTML += "&#X1F3C1";
+  */
 }
