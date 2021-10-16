67be568c8c37bb7b87f4015aac5a83c386a719a7
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-15 21:22:04 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,17 +5,16 @@ function contar() {
   var a = Number(ini.value);
   var b = Number(fim.value);
   var c = Number(passo.value);
-  var d = 0;
   var res = document.querySelector("div#res");
   if (ini.value.length == 0) {
     res.innerHTML = "[ERRO] impossível contar sem início!";
   } else if (Number(passo.value) <= 0 || passo.value.length == 0) {
     res.innerHTML = "[ERRO] informe passo válido!";
   } else {
-    do {
-      a + c;
+    for (var cont = 0; d <= b; d++) {
+      a += c;
       res.innerHTML += `${a} &#X1F449`;
-    } while (a <= b);
+    }
   }
   res.innerHTML += "&#X1F3C1";
 }
