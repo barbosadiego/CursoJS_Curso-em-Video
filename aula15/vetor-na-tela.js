922c7735345740d4231b17e2427c05edce8d2ba9
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-18 11:48:12 -0300

    Update vetor-na-tela.js

--- aula15/vetor-na-tela.js
+++ aula15/vetor-na-tela.js
@@ -1,8 +1,18 @@
 let valores = [8,1,7,4,2,9]
+valores.sort()
 
+//forma mais simples nas versões mais recentes da ECMAScript
+for(let pos in valores){
+  console.log(`A posição ${pos} possui o valor ${valores[pos]}`)
+}
+
+
+//percurso para exibição do vetor
+/*
 for(let pos = 0; pos < valores.length; pos++){
   console.log(`A posição ${pos} possui o valor ${valores[pos]}`)
 }
+*/
 
 //console.log(valores)
 
