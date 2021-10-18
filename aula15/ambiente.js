852996dda782c1d7b0946dc43f4e22430371059e
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-18 11:35:59 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,9]
+num[4]=1
+num.push(7)
+num.sort()
+
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos ou posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
